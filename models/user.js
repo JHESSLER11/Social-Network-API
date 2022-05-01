5dc5ffe01c6eca81fd39590363dc882faaf5bc9d
JHessler <jhessler11@gmail.com>
2022-05-01 14:59:35 -0400

    user model

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+
+    {
+        userName: {
+            type: string,
+            unique: true,
+            require: 'User name is required',
+            trim: true
+        },
+
+        email: {
+            type: string,
+            unique: true,
+            require: 'email is required',
+            trim: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thought: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'though'
+            }
+        ],
+        friends: [userSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+model.exports = User;
