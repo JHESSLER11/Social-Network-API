0e55c4ec548a585c4a9be98eca474037f7f537e2
JHessler <jhessler11@gmail.com>
2022-05-07 15:57:10 -0400

    create thought working

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,6 +2,30 @@ const { Thought, User } = require('../models')
 
 const thoughtController  = {
 
+    getAllThoughts(req, res) {
+        Thought.find({})
+        // .populate({path: 'users', select: '-__v'})
+        // .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(dbThoughtData => {
+            User.findOneAndUpdate(
+                { _id: body.id },
+                { $push: { thoughts: dbThoughtData._id }}
+            )
+        })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
+    },
 }
 
 
